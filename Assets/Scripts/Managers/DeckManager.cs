26590ba0aadf6d5c665d0728b51ae13da93fda63
umiii_777-admin <skylinee717@gmail.com>
2025-11-19 00:24:05 +0800

    增加牌库抽卡表现

--- Assets/Scripts/Managers/DeckManager.cs
+++ Assets/Scripts/Managers/DeckManager.cs
@@ -13,7 +13,7 @@ public class DeckManager : MonoBehaviour
     public int currentLevelDeckTotalNum;  //当前关卡的牌库卡牌持有数量
 
     private CardData currentCardData;
-    private List<CardData> deckCardDatas;
+    private List<CardData> currentDeckCardDatas;
 
     private List<Card> currentSetCards;
 
@@ -21,13 +21,13 @@ public class DeckManager : MonoBehaviour
     private int DeckSetNum;
 
     //参数设置
-    public Vector2 firstPresetPos = new Vector2(350,-780);
+    private Vector2 firstPresetPos = new Vector2(0, -780);
 
     private int presetNumMax = 3;//预备区域的最大张数量
     private int presetOffset = 50;//预备区域的偏移量
     private int currentPresetNum = 0;//当前预备区域的卡牌数
 
-    
+
     private void Awake()
     {
         Instance = this;
@@ -37,38 +37,44 @@ public class DeckManager : MonoBehaviour
     //每关卡开始时，初始化牌库
     public void InitCardDeck(List<CardData> currentFullDeckDatas)
     {
-
-        deckCardDatas = currentFullDeckDatas;
-        currentLevelDeckTotalNum = deckCardDatas.Count;
+        currentDeckCardDatas = currentFullDeckDatas;
+        currentLevelDeckTotalNum = currentFullDeckDatas.Count;
         Debug.Log("当前的deck中有" + currentLevelDeckTotalNum + "张卡牌");
     }
 
     public void DrawCard()
     {
+        //当牌库已经被取出数据数量
         if (DeckSetNum > currentLevelDeckTotalNum)
         {
             ResetDeck();
         }
         else
         {
-            GameObject CardObj = cardPool.Get();
-            Card currentCard = CardObj.GetComponent<Card>();
-            currentCard.cardData = deckCardDatas[currentDeckCount];
-            currentCard.SetCardVisual();
-
-            RectTransform currentCardRect = CardObj.GetComponent<RectTransform>();
-            currentCardRect.SetParent(canvas, false);
-            currentCardRect.anchoredPosition = firstPresetPos;
+            GameObject cardObj = cardPool.Get();
+            SetACardFromDeck(cardObj);
             //currentCardRect.SetParent(dragla)
 
 
-            
+
         }
+    }
+    private void SetACardFromDeck(GameObject cardObj)
+    {
+        Card currentCard = cardObj.GetComponent<Card>();
+        currentCard.isFront = true;
+        currentCard.cardData = currentDeckCardDatas[currentDeckCount];
+        //currentDeckCardDatas.Remove(currentCard.cardData);      //从当前的数据库中移出这个数据
+        currentCard.SetCardVisual();
+        RectTransform currentCardRect = cardObj.GetComponent<RectTransform>();
+        currentCardRect.SetParent(canvas, false);
+        currentCardRect.anchoredPosition = firstPresetPos;
+        currentDeckCount++;
     }
     public void ResetDeck()
     {
-        
+
     }
-    
+
 
 }
