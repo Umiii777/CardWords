d6d273f4180f9550d4cd56e915cf7b29f5e4196f
ZJWCY <68607421+ZJWCY@users.noreply.github.com>
2026-01-09 03:54:44 +0900

    [update] 添加 DailyCheckInUIController.fs；引入 F#

---
+++ Assets/Libraries/ZFSharp/DailyCheckInUIController.fs
@@ -0,0 +1,13 @@
+﻿namespace ZFSharp
+
+open UnityEngine
+
+type DailyCheckInController =
+    inherit StaticAdProcessor<DailyCheckInController, Object>
+
+    member _.Start() =
+        task {
+            do! Async.Sleep 3000
+            do! SystemUIManager.PopUpTips "Hello, F#!"
+        } |> ignore
+        Debug.Log "Hello, F#!"
